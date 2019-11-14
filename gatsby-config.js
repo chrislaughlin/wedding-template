d15d6f555a0c74fe4f76100ea176ca825ec1879f
Chris Laughlin <chris_laughlin@rapid7.com>
2019-11-14 22:25:58 +0000

    basic version

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,27 +6,15 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `@agney/gatsby-theme-wedding`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+        contentPath: "occasion", // Tell the theme where your data is.
+        basePath: "/" // Tell the theme which URL to render the theme at.
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
